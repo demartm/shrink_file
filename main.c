f47e4cd23ba0c41ab2c1748037e40b3fe2148e8f
demartm <artemis20242511@gmail.com>
2026-04-27 11:04:31 -0400

    char -> unsigned char

--- main.c
+++ main.c
@@ -2,40 +2,57 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void shrink_file(FILE *a,FILE *b){
+void pack_file(FILE *a,FILE *b){
 
-char *buff = (char*)calloc(1000,sizeof(char));
+if(a && b){
 
-if(a){
+  unsigned char *buff = (unsigned char*)calloc(7,sizeof(unsigned char));
 
-  size_t read = 0;
+    if(buff){
+      size_t read = 0;
 
-  while((read = fread(buff,1,7,a)) == 7){
+      while((read = fread(buff,1,7,a)) == 7){
 
-      unsigned char mask = 1;
+          unsigned char mask = 1;
 
-      for(int j = 1; j < 7; j++ ){
+          for(int j = 1; j < 7; j++ ){
 
-        if(buff[0] & mask){
+            if(buff[0] & mask){
 
-          buff[j] = buff[j] | 128;//10000000b
+              buff[j] = buff[j] | 128;//10000000b
+            }
+
+            mask = mask << 1;
+          }
+
+          fwrite(&buff[1],1,6,b);
+
         }
 
-        mask = mask << 1;
+      if(read > 0){
+
+        fwrite(buff,1,read,b);
       }
 
-      fwrite(&buff[1],1,6,b);
 
     }
+}
 
-  if(read > 0){
+}
 
-    fwrite(buff,1,read,b);
-  }
+void unpack_file(FILE *a, FILE *b){
+
+if(a && b){
+
+
+
 
 
 }
+
+
 }
+
 
 int main()
 {
@@ -49,7 +66,7 @@ b = fopen("/home/root_user/shrink_file/test2.txt","wb+");
 
 if(b){
 
-shrink_file(a,b);
+pack_file(a,b);
 
 }
 
