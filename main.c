25596ff2a4415d148b3e787f570759a138534180
demartm <artemis20242511@gmail.com>
2026-04-27 11:29:29 -0400

    unpack function

--- main.c
+++ main.c
@@ -44,8 +44,37 @@ void unpack_file(FILE *a, FILE *b){
 
 if(a && b){
 
+  unsigned char *buff = (unsigned char*)calloc(7,sizeof(unsigned char));
+
+  if(buff){
+
+    size_t read = 0;
+
+    while((read = fread(&buff[1],1,6,a)) == 6){
+      buff[0] = 0;
+      unsigned char mask = 128;
+
+      for(int i = 1; i < 7; i++){
+
+        buff[0] = buff[0] | (buff[i] & mask);
+        buff[0] = buff[0] >> 1;
+
+        buff[i] = buff[i] & (~mask);
+
+      }
+
+      buff[0] = buff[0] >> 1;
+
+      fwrite(buff,1,7,b);
+
+    }
+
+    if(read > 0){
+      fwrite(buff,1,read,b);
+    }
 
 
+  }
 
 
 }
@@ -68,11 +97,23 @@ if(b){
 
 pack_file(a,b);
 
-}
-
-}
-
 fclose(a);
 fclose(b);
+
+FILE *c = fopen("/home/root_user/shrink_file/test2.txt","rb");
+FILE *d = fopen("/home/root_user/shrink_file/test3.txt","wb+");
+
+if(c && d){
+unpack_file(c,d);
+
+}
+fclose(a);
+fclose(b);
+
+}
+
+}
+
+
 return 0;
 }
