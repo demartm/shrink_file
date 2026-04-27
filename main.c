399baa8dcf250111b85a8852355e4421d9dd0ee0
demartm <artemis20242511@gmail.com>
2026-04-27 10:39:19 -0400

    shrink prototype

--- main.c
+++ main.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main()
 {
-FILE *a;
-a = fopen("/home/root_user/shrink_file/main.c","r");
-char *buff = (char*)calloc(100,sizeof(char));
+FILE *a, *b;
+
+a = fopen("/home/root_user/shrink_file/test.txt","r");
+b = fopen("/home/root_user/shrink_file/test2.txt","w+");
+
+char *buff = (char*)calloc(1000,sizeof(char));
+
 if(a){
 
-while(fread(buff,1,1,a) > 0){
-printf("1");
-}
+  for(int i = 0; fread(buff,1,7,a) > 0; i += 7){
+
+    unsigned char mask = 1;
+
+    for(int j = 1; j < 7; j++ ){
+
+      if(buff[0] & mask){
+
+        buff[j] = buff[j] | 128;//10000000b
+      }
+
+      mask = mask << 1;
+    }
+
+    fwrite(&buff[1],1,6,b);
+
+  }
+
 
 }
- //   printf("Hello World!\n");
-    return 0;
+
+return 0;
 }
