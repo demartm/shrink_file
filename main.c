37528f2b5edaa40a97c27948048be365086cec4c
demartm <artemis20242511@gmail.com>
2026-04-27 12:40:15 -0400

    comparison as a function

--- main.c
+++ main.c
@@ -2,6 +2,28 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+bool compare(FILE *a, FILE *b){
+
+if(a && b){
+  unsigned char buf_a = 0;
+  unsigned char buf_b = 0;
+
+  bool is_end =false;
+
+  while(fread(&buf_a,1,1,a) && fread(&buf_b,1,1,b)){
+
+    if(buf_a != buf_b){
+
+      return false;
+    }
+
+  }
+  return true;
+}
+return false;
+
+}
+
 int main()
 {
 
@@ -9,19 +31,10 @@ FILE *a, *b;
 a= fopen("test.txt","rb");
 b= fopen("test3.txt","rb");
 
-unsigned char buf_a = 0;
-unsigned char buf_b = 0;
-
-bool is_end =false;
-
-while(fread(&buf_a,1,1,a) && fread(&buf_b,1,1,b) && !is_end){
-  if(buf_a != buf_b){
-    printf("error");
-    is_end = true;
-  }
+if(!compare(a,b)){
+printf("error");
 }
-fclose(a);
-fclose(b);
 
     return 0;
+
 }
