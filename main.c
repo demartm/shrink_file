80ee92302dc5059cb1b2cffd204df412df0def78
demartm <artemis20242511@gmail.com>
2026-05-04 17:47:02 -0400

    cleaned master

--- main.c
+++ main.c
@@ -1,18 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-FILE *a;
-a = fopen("/home/root_user/shrink_file/main.c","r");
-char *buff = (char*)calloc(100,sizeof(char));
-if(a){
 
-while(fread(buff,1,1,a) > 0){
-printf("1");
-}
-
-}
- //   printf("Hello World!\n");
-    return 0;
-}
