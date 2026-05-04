20b954dc39df941feb7b3a504af5ddd5a2869564
demartm <artemis20242511@gmail.com>
2026-05-04 17:39:38 -0400

    test added

--- main.c
+++ main.c
@@ -1,46 +1,54 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-bool pack_file(char *file_a,char *file_b, size_t *len){
-FILE *a  = fopen(file_a,"rb");//NULL;
-FILE *b = fopen(file_b,"wb");//NULL;
+int pack_file(char *file_a,char *file_b){
 
+FILE *a  = fopen(file_a,"rb");
+int ret_code = 0;
+bool is_ok = true;
 if(!a){
 
-  return 0;
+  return 1;
 }
+FILE *b = fopen(file_b,"wb");
 if(!b){
 
-  fclose(a);
-  return 0;
-}
+  if(fclose(a) != 0){
+    return 2;
+  }
 
+  return 1;
+}
+  fseek(a,0,SEEK_END);
+  int size = ftell(a);
+  fseek(a,0,SEEK_SET);
+  unsigned char tail = size%8;
+  if(size != 0) {
+  if(fprintf(b,"%c",tail) == 1) {
   unsigned char *buff = (unsigned char*)calloc(8,sizeof(unsigned char));
 
     if(buff){
       size_t read = 0;
 
-      while ((read = fscanf(a,"%c",buff)) == 1){
+      while ((read = fscanf(a,"%c",buff)) == 1 && is_ok == 1){
 
-              if(buff[0] & 128){
+          if(buff[0] & 128){
+            ret_code = 3;
+            is_ok = false;
+          }else{
+      for(int i = 1; i < 8 && fscanf(a,"%c",(unsigned char*)&buff[i]) == 1 && is_ok;i++){
+         if(buff[i] & 128){
+            ret_code = 3;
+            is_ok = false;
+            i = 8;
 
-                fclose(a);
-                fclose(b);
-                return false;
-              }
-
-      for(int i = 1; i < 8 && fscanf(a,"%c",&buff[i]) == 1;i++){
-              if(buff[i] & 128){
-
-                fclose(a);
-                fclose(b);
-                return false;
-              }
+              }else{
         read++;
       }
-
+}
+}
+if(is_ok){
       if(read == 8){
           unsigned char mask = 1;
 
@@ -58,12 +66,10 @@ if(!b){
         for(int i = 1;i < 8;i++){
 
           if(fprintf(b,"%c",buff[i]) != 1) {
+            ret_code = 4;
+            is_ok = false;
+            i = 8;
 
-            fclose(a);
-            fclose(b);
-            return false;
-          }else{
-            (*len)++;
           }
         }
 
@@ -73,8 +79,10 @@ if(!b){
         for(int i = 0; i < read; i++){
 
           if(fprintf(b,"%c",buff[i]) != 1){
+            ret_code = 4;
+            is_ok = false;
+            i = read;
 
-            return false;
            }
 
         }
@@ -82,38 +90,60 @@ if(!b){
       }
       }
 }
-
-  fclose(a);
-  fclose(b);
-  free(buff);
-
-  return true;
-
-}
-//}
-fclose(a);
-fclose(b);
-
-return false;
 }
 
-bool unpack_file(char *file_a,char *file_b, size_t len){
+free(buff);
+} else {
+  ret_code = 7;
+}
+
+}
+} else {
+ret_code = 10;
+}
+if(fclose(b) != 0){
+
+  ret_code = 6;
+}
+if(ret_code){
+  remove(file_b);
+}
+
+if(fclose(a) != 0){
+  ret_code = 6;
+}
+
+return ret_code;
+}
+
+int unpack_file(char *file_a,char *file_b){
 FILE *a  = fopen(file_a,"rb");//NULL;
-FILE *b = fopen(file_b,"wb");//NULL;
-//size_t len = 0;
 
+bool is_ok = true;
+int ret_code = 0;
 
 if(!a){
 
-  return 0;
+  return 1;
 }
+FILE *b = fopen(file_b,"wb");//NULL;
 if(!b){
 
-  fclose(a);
-  return 0;
-}
-if(a && b){
+  if(fclose(a) != 0){
+    return 2;
+  }
 
+  return 1;
+}
+//if(a && b){
+fseek(a,0,SEEK_END);
+size_t len = ftell(a);
+fseek(a,0,SEEK_SET);
+unsigned char tail = 0;
+if(len != 0) {
+if(fscanf(a,"%c",(unsigned char*)&tail) == 1){
+if(tail < 8){
+len = len - (int)tail - 1;
   unsigned char *buff = (unsigned char*)calloc(8,sizeof(unsigned char));
 
   if(buff){
@@ -121,12 +151,12 @@ if(a && b){
     size_t ix = 0;
   unsigned char mask = 128;//10000000b
 
-  while(ix < len){
+  while(ix < len && is_ok == 1){
 
   buff[0] = 0;
 
-  for(int i = 1; i < 8; i++){
-    if(fscanf(a,"%c",&buff[i]) == 1){
+  for(int i = 1; i < 8 && is_ok; i++){
+    if(fscanf(a,"%c",(unsigned char*)&buff[i]) == 1){
 
       buff[0] = buff[0] | (buff[i] & mask);
       buff[0] = buff[0] >> 1;
@@ -134,45 +164,56 @@ if(a && b){
       buff[i] = buff[i] & (~mask);
       ix++;
     } else {
+      ret_code = 3;
+      is_ok = false;
 
-      fclose(a);
-      fclose(b);
-      return false;
     }
   }
 
-  for(int i = 0; i < 8; i++){
+  for(int i = 0; i < 8 && is_ok; i++){
 
     if(fprintf(b,"%c",buff[i]) != 1){
+      ret_code = 4;
+      is_ok = false;
 
-      fclose(a);
-      fclose(b);
-      return 0;
     }
   }
 }
-  while(fscanf(a,"%c",buff) == 1){
+  while(fscanf(a,"%c",buff) == 1 && is_ok){
 
       if(!fprintf(b,"%c",buff[0])){
+        ret_code = 4;
+        is_ok = false;
 
-        fclose(a);
-        fclose(b);
-        return 0;
       }
   }
 
   free(buff);
-  fclose(a);
-  fclose(b);
 
-  return true;
+} else {
+ret_code = 5;
+}
+
+
 }
 
 }
+} else {
+  ret_code = 7;
+}
 
-fclose(a);
-fclose(b);
-return false;
+if(fclose(b) != 0){
+
+  ret_code = 6;
+}
+if(ret_code){
+  remove(file_b);
+}
+if(fclose(a) != 0){
+  ret_code = 6;
+}
+
+return ret_code;
 }
 
 
@@ -198,8 +239,8 @@ if(!b){
   bool is_end = false;
   while(!is_end){
 
-  n1 = fscanf(a,"%c",&buf_a);
-  n2 = fscanf(b,"%c",&buf_b);
+  n1 = fscanf(a,"%c",(unsigned char*)&buf_a);
+  n2 = fscanf(b,"%c",(unsigned char*)&buf_b);
 
   if((n1 == 1) || (n2 == 1)){
 
@@ -231,25 +272,36 @@ if(!b){
 
 int main()
 {
-size_t len = 0;
-char test1[] = "test.txt";
+char text[] = "helloworld12345\0";
+char test1[] = "test01.txt";//"test02.txt";//"test03.txt";//"test04.txt";//"test05.txt";//"test06.txt";//"test07.txt";//"test08.txt";
+//char test1[] = "test.txt";
 char test2[] = "test2.txt";
 char test3[] = "test3.txt";
+//  FILE *a = fopen(test1,"r");
+// // fwrite(text,1,sizeof(text)-1,a);
 
-if(!pack_file(test1,test2,&len)){
+// fseek(a,0,SEEK_END);
+// len = ftell(a);
+// fseek(a,0,SEEK_END);
+// printf("%d",len);
+// fclose(a);
+// return 0;
 
-  printf("error packing file");
+ // fclose(a);
+int code = 0;
+if((code = pack_file(test1,test2)) != 0){
+
+  printf("error packing file. code: %d",code);
   return 0;
 } else{
 
-  printf("%d",len);
   printf("\nfile packed successfully");
 }
 
 
-if(!unpack_file(test2,test3,len)){
+if((code = unpack_file(test2,test3)) != 0){
 
-  printf("\nerror unpacking file");
+  printf("\nerror unpacking file. code: %d",code);
   return 0;
 }
 
